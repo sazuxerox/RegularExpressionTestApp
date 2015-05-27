6ad8144fe2d9de9fb88f9563eaa77880421a63c5
Sazzad Hossain <sazu1007@gmail.com>
2015-05-27 23:38:55 +0600

    initial commit for RegularExpressionTest Program

---
+++ RegularExpressionMatchingTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionMatchingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //defining some word for test 
+            string[] testWord = { "14522-asAdE", "getAE-12343", "ERdee-qw e",};
+
+            Regex regex = new Regex(@"\d-\w");
+
+            foreach (var item in testWord)
+            {
+                Console.WriteLine("{0} {1} a valid match", item, regex.IsMatch(item)? "is" : "is not");
+            }
+        }
+    }
+}
